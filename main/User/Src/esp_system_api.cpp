26e530c375590fbe5f313d7a2090996c3a534a05
Thanh Tùng <electricthanhtung@gmail.com>
2024-05-20 00:49:36 +0700

    Complete implementation for MjvmSystem_Write function

--- main/User/Src/esp_system_api.cpp
+++ main/User/Src/esp_system_api.cpp
@@ -1,22 +1,31 @@
 
 #include <stdio.h>
+#include "mjvm_string.h"
 #include "mjvm_system_api.h"
 
 void MjvmSystem_Write(const char *text, uint32_t length, uint8_t coder) {
     char buff[64];
     if(coder == 0) {
         while(length) {
-            uint32_t tmp = length < sizeof(buff) ? length : (sizeof(buff) - 1);
-            length -= tmp;
-            for(uint32_t i = 0; i < tmp; i++)
-                buff[i] = text[i];
-            text += tmp;
-            buff[tmp] = 0;
-            fprintf(stdout, buff);
+            uint32_t count = 0;
+            while(length && ((count + 2) <= sizeof(buff))) {
+                count += MjvmString::utf8Encode((uint8_t)*text, &buff[count]);
+                text++;
+                length--;
+            }
+            fprintf(stdout, "%*.*s", (int)count, (int)count, buff);
         }
     }
     else {
-        // TODO
+        while(length) {
+            uint32_t count = 0;
+            while(length && ((count + 3) <= sizeof(buff))) {
+                count += MjvmString::utf8Encode(*(uint16_t *)text, &buff[count]);
+                text += 2;
+                length--;
+            }
+            fprintf(stdout, "%*.*s", (int)count, (int)count, buff);
+        }
     }
 }
 
