7dbf1577a0b02df10dee2bf91faeecbb86a2d279
Thanh Tùng <electricthanhtung@gmail.com>
2024-08-10 14:05:20 +0700

    Update FlintJVM to support console buffer in debugger

--- main/User/Inc/flint_conf.h
+++ main/User/Inc/flint_conf.h
@@ -4,12 +4,13 @@
 
 #include "flint_common.h"
 
-#define FILE_NAME_BUFF_SIZE     256
+#define FILE_NAME_BUFF_SIZE         256
 
-#define DEFAULT_STACK_SIZE      KILO_BYTE(10)
-#define OBJECT_SIZE_TO_GC       KILO_BYTE(10)
+#define DEFAULT_STACK_SIZE          KILO_BYTE(10)
+#define OBJECT_SIZE_TO_GC           KILO_BYTE(10)
 
-#define MAX_OF_BREAK_POINT      20
-#define MAX_OF_DBG_BUFFER       512
+#define MAX_OF_BREAK_POINT          20
+#define DBG_TX_BUFFER_SIZE          512
+#define DBG_CONSOLE_BUFFER_SIZE     KILO_BYTE(1)
 
 #endif /* __FLINT_CONF_H */
