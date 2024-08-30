0957a4faca4974b1a6387abf023c428c5ce9aa3a
Thanh Tùng <electricthanhtung@gmail.com>
2024-08-30 10:04:04 +0700

    Implement Directory::open, Directory::read and Directory::close

--- main/User/Src/esp_system_directory_api.cpp
+++ main/User/Src/esp_system_directory_api.cpp
@@ -2,6 +2,47 @@
 #include "esp_vfs_fat.h"
 #include "flint_system_api.h"
 
+void *FlintAPI::Directory::open(const char *dirName) {
+    FF_DIR *dir = (FF_DIR *)FlintAPI::System::malloc(sizeof(FF_DIR));
+    FRESULT ret = f_opendir(dir, dirName);
+    if(ret == FR_OK)
+        return (void *)dir;
+    else {
+        FlintAPI::System::free(dir);
+        return 0;
+    }
+}
+
+FlintFileResult FlintAPI::Directory::read(void *handle, bool *isFile, char *nameBuff, uint32_t buffSize) {
+    FILINFO fno;
+    FRESULT ret = f_readdir((FF_DIR *)handle, &fno);
+    if(ret == FR_OK && fno.fname[0]) {
+        uint16_t index = 0;
+        while(fno.fname[index]) {
+            if(index < buffSize) {
+                nameBuff[index] = fno.fname[index];
+                index++;
+            }
+            else
+                return FILE_RESULT_ERR;
+        }
+        nameBuff[index] = 0;
+        return FILE_RESULT_OK;
+    }
+    else if((ret == FR_NO_PATH) || (ret == FR_NO_FILE) || (ret == FR_INVALID_NAME))
+        return FILE_RESULT_NO_PATH;
+    else if(ret == FR_DENIED)
+        return FILE_RESULT_DENIED;
+    else
+        return FILE_RESULT_ERR;
+}
+
+FlintFileResult FlintAPI::Directory::close(void *handle) {
+    FRESULT ret = f_closedir((FF_DIR *)handle);
+    FlintAPI::System::free(handle);
+    return (ret == FR_OK) ? FILE_RESULT_OK : FILE_RESULT_ERR;
+}
+
 FlintFileResult FlintAPI::Directory::exists(const char *path) {
     FRESULT ret = f_stat(path, NULL);
     if(ret == FR_OK)
