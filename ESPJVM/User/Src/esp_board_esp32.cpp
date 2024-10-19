fa9c1087679dcc46ff4c03a55acce14853de96aa
Thanh Tùng <electricthanhtung@gmail.com>
2024-10-19 22:43:30 +0700

    Remove compiler warnings

--- ESPJVM/User/Src/esp_board_esp32.cpp
+++ ESPJVM/User/Src/esp_board_esp32.cpp
@@ -23,14 +23,13 @@ static void NVS_Init(void) {
 }
 
 static void UART_Init(uart_port_t uartNum, int32_t baudRate) {
-    uart_config_t uart_config = {
-        .baud_rate = baudRate,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(uartNum, &uart_config);
+    uart_config_t uartConfig = {};
+    uartConfig.baud_rate = baudRate;
+    uartConfig.data_bits = UART_DATA_8_BITS;
+    uartConfig.parity    = UART_PARITY_DISABLE;
+    uartConfig.stop_bits = UART_STOP_BITS_1;
+    uartConfig.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+    uart_param_config(uartNum, &uartConfig);
     uart_set_pin(uartNum, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 }
 
