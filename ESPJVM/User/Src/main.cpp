16bb76ed8f3f633daf1a5eefb047118ab14c33a4
Thanh Tùng <electricthanhtung@gmail.com>
2025-05-12 21:44:42 +0700

    Check reset reason

--- ESPJVM/User/Src/main.cpp
+++ ESPJVM/User/Src/main.cpp
@@ -1,7 +1,8 @@
 
 #include <stdio.h>
-#include "esp_vfs_fat.h"
 #include "flint.h"
+#include "esp_system.h"
+#include "esp_vfs_fat.h"
 #include "esp_board.h"
 #include "esp_debugger.h"
 
@@ -21,8 +22,12 @@ extern "C" void app_main() {
 
     Flint &flint = Flint::getInstance();
     EspDebugger &dbg = EspDebugger::getInstance(flint);
-    if(FlintAPI::IO::finfo("main.class", NULL, NULL) == FILE_RESULT_OK)
-        flint.runToMain("main");
+    
+    if(esp_reset_reason() != ESP_RST_PANIC) {
+        if(FlintAPI::IO::finfo("main.class", NULL, NULL) == FILE_RESULT_OK)
+            flint.runToMain("main");
+    }
+
     vTaskPrioritySet(NULL, 2);
     dbg.receiveTask();
 }
