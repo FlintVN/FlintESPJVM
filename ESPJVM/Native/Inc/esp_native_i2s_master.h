e94e7144b54f3043c8d2a597333e829b8822e3a8
Thanh Tùng <electricthanhtung@gmail.com>
2025-12-05 18:38:07 +0700

    Updated I2sMaster to match the new FlintJDK update

--- ESPJVM/Native/Inc/esp_native_i2s_master.h
+++ ESPJVM/Native/Inc/esp_native_i2s_master.h
@@ -10,19 +10,26 @@ void NativeI2sMaster_Reset(void);
 
 jobject NativeI2sMaster_Open(FNIEnv *env, jobject obj);
 jbool NativeI2sMaster_IsOpen(FNIEnv *env, jobject obj);
+
 jint NativeI2sMaster_ReadByte(FNIEnv *env, jobject obj);
-jint NativeI2sMaster_Read(FNIEnv *env, jobject obj, jbyteArray b, jint off, jint count);
+jint NativeI2sMaster_ReadByteArray(FNIEnv *env, jobject obj, jbyteArray b, jint off, jint count);
+jint NativeI2sMaster_ReadShortArray(FNIEnv *env, jobject obj, jshortArray b, jint off, jint count);
+jint NativeI2sMaster_ReadIntArray(FNIEnv *env, jobject obj, jintArray b, jint off, jint count);
+
 jvoid NativeI2sMaster_WriteByte(FNIEnv *env, jobject obj, jint b);
-jvoid NativeI2sMaster_Write(FNIEnv *env, jobject obj, jbyteArray b, jint off, jint count);
+jvoid NativeI2sMaster_WriteByteArray(FNIEnv *env, jobject obj, jbyteArray b, jint off, jint count);
+jvoid NativeI2sMaster_WriteShortArray(FNIEnv *env, jobject obj, jshortArray b, jint off, jint count);
+jvoid NativeI2sMaster_WriteIntArray(FNIEnv *env, jobject obj, jintArray b, jint off, jint count);
+
 jvoid NativeI2sMaster_Close(FNIEnv *env, jobject obj);
 
 static constexpr NativeMethod i2sMasterMethods[] = {
     NATIVE_METHOD("open",   "()Lflint/machine/I2sMaster;", NativeI2sMaster_Open),
     NATIVE_METHOD("isOpen", "()Z",                         NativeI2sMaster_IsOpen),
     NATIVE_METHOD("read",   "()I",                         NativeI2sMaster_ReadByte),
-    NATIVE_METHOD("read",   "([BII)I",                     NativeI2sMaster_Read),
+    NATIVE_METHOD("read",   "([BII)I",                     NativeI2sMaster_ReadByteArray),
     NATIVE_METHOD("write",  "(I)V",                        NativeI2sMaster_WriteByte),
-    NATIVE_METHOD("write",  "([BII)V",                     NativeI2sMaster_Write),
+    NATIVE_METHOD("write",  "([BII)V",                     NativeI2sMaster_WriteByteArray),
     NATIVE_METHOD("close",  "()V",                         NativeI2sMaster_Close),
 };
 
