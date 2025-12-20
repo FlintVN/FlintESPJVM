1170f2ae13b215d16baea3267b48bc0b99f59795
Thanh Tùng <electricthanhtung@gmail.com>
2025-12-20 13:51:36 +0700

    Fixes a critical bug in NativePin_Reset that caused debugging to stop working on the ESP32_S3NxRx board

--- ESPJVM/Native/Src/esp_native_pin.cpp
+++ ESPJVM/Native/Src/esp_native_pin.cpp
@@ -15,13 +15,25 @@ const char *NativePin_CheckPin(int32_t pin) {
         return "Pin 1 and 3 have been used for debugging";
     else if((6 <= pin) && (pin <= 11))
         return "Pins 6 to 11 have been used for SPI Flash";
-#elif CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
+#elif CONFIG_IDF_TARGET_ESP32S2
     else if(pin == 0)
         return "Pin 0 was used to select USB mode";
     else if((pin == 19) || (pin == 20))
         return "Pin 19 and 20 have been used for debugging (USB CDC)";
     else if((26 <= pin) && (pin <= 32))
         return "Pins 26 to 32 have been used for SPI Flash and PSRAM";
+#elif CONFIG_IDF_TARGET_ESP32S3
+    else if(pin == 0)
+        return "Pin 0 was used to select USB mode";
+    else if((pin == 19) || (pin == 20))
+        return "Pin 19 and 20 have been used for debugging (USB CDC)";
+#ifdef ESP32_S3FH4R2
+    else if((26 <= pin) && (pin <= 32))
+        return "Pins 26 to 32 have been used for SPI Flash and PSRAM";
+#else
+    else if((26 <= pin) && (pin <= 37))
+        return "Pins 26 to 37 have been used for SPI Flash and PSRAM";
+#endif /* ESP32_S3FH4R2 */
 #elif CONFIG_IDF_TARGET_ESP32C3
     else if(pin == 18 || pin == 19)
         return "Pin 18 and 19 have been used for debugging (USB-JTAG)";
